f5e53074f94e8a33101c17d63195f885de22f6fe
clientesweb <alexisifran32@gmail.com>
2024-09-24 23:44:44 -0300

    Actualizar scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -39,32 +39,7 @@ nextProdBtn.addEventListener('click', () => {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navbar = document.querySelector('.navbar');
     const bottomNavItems = document.querySelectorAll('.bottom-nav-item');
-
-    menuToggle.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-        this.classList.toggle('active');
-
-        if (this.classList.contains('active')) {
-            this.setAttribute('aria-expanded', 'true');
-        } else {
-            this.setAttribute('aria-expanded', 'false');
-        }
-    });
-
-    // Cerrar el menú al hacer clic en un enlace (para móviles)
-    const navLinks = document.querySelectorAll('.navbar a, .bottom-nav-item');
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            if (window.innerWidth <= 768) {
-                navbar.classList.remove('active');
-                menuToggle.classList.remove('active');
-                menuToggle.setAttribute('aria-expanded', 'false');
-            }
-        });
-    });
 
     // Actualizar el ítem activo en el menú inferior
     bottomNavItems.forEach(item => {
@@ -73,15 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
             bottomNavItems.forEach(navItem => navItem.classList.remove('active'));
             item.classList.add('active');
         });
-    });
-
-    // Manejar cambios de tamaño de ventana
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 768) {
-            navbar.classList.remove('active');
-            menuToggle.classList.remove('active');
-            menuToggle.setAttribute('aria-expanded', 'false');
-        }
     });
 
     // Manejar el desplazamiento para resaltar el ítem activo en el menú inferior
