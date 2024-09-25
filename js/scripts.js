2595ef7cd80489d23dcb0a5628bec541ed9fb28d
clientesweb <alexisifran32@gmail.com>
2024-09-25 02:56:05 -0300

    Actualizar scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -101,3 +101,12 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+// Funcionalidad para "Añadir al Carrito"
+const botonesComprar = document.querySelectorAll('.btn-comprar');
+
+botonesComprar.forEach(boton => {
+    boton.addEventListener('click', () => {
+        alert('Producto añadido al carrito');
+        // Aquí puedes añadir más lógica para gestionar el carrito.
+    });
+});
