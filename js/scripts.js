6115e8d93e10b2fd2b82a2f72a1be48d03f0e06f
clientesweb <alexisifran32@gmail.com>
2024-10-02 11:35:00 -0300

    Actualizar scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ function loadProductSlider(category = 'all') {
                 <div class="p-4">
                     <h3 class="text-lg font-semibold mb-2">${product.name}</h3>
                     <p class="text-gray-600">$${product.price.toFixed(2)}</p>
-                    <button class="mt-4 bg-primary text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300" onclick="addToCart(${product.id})">Agregar al carrito</button>
+                    <button class="mt-4 bg-[#EAE4D2] text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300" onclick="addToCart(${product.id})">Agregar al carrito</button>
                 </div>
             `;
             productSlider.appendChild(productElement);
@@ -51,7 +51,7 @@ function loadFeaturedProducts() {
             <div class="p-4">
                 <h3 class="text-lg font-semibold mb-2">${product.name}</h3>
                 <p class="text-gray-600">$${product.price.toFixed(2)}</p>
-                <button class="mt-4 bg-primary text-white px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300" onclick="addToCart(${product.id})">Agregar al carrito</button>
+                <button class="mt-4 bg-primary text-white px-4 py-2 rounded-full hover:bg-[#EAE4D2] transition duration-300" onclick="addToCart(${product.id})">Agregar al carrito</button>
             </div>
         `;
         featuredSlider.appendChild(productElement);
